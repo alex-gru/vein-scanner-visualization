43023bff9f4eebf334080225086f878a6958087a
AlexGru <grusch.alex@gmail.com>
2014-03-25 12:57:33 +0100

    cut out rectangle is shown now, file saving in progress

--- src/lndf/Main.java
+++ src/lndf/Main.java
@@ -1,18 +1,23 @@
 package lndf;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.RectangleBuilder;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Collection;
+import java.io.IOException;
 
 /**
  * Created by alexgru-mobile on 18.03.14.
@@ -20,6 +25,8 @@ import java.util.Collection;
 public class Main extends Application {
     private static final int DIM_X = 800;
     private static final int DIM_Y = 500;
+    private String currOriginName;
+
     private Image capturedImageSrc;
     private ImageView capturedImage;
     private double cutOutStartX;
@@ -28,61 +35,80 @@ public class Main extends Application {
     private double cutOutEndY;
     private Rectangle cutOutRegion;
 
+    private EventHandler<? super MouseEvent> mousePressedHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            System.out.println("CLICK!");
+            cutOutStartX = mouseEvent.getX();
+            cutOutStartY = mouseEvent.getY();
+        }
+    };
+    private EventHandler<? super MouseEvent> mouseDraggedHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            System.out.println("DRAGGED!");
+            cutOutEndX = mouseEvent.getX();
+            cutOutEndY = mouseEvent.getY();
+
+            cutOutRegion.setX(capturedImage.getX() + cutOutStartX);
+            cutOutRegion.setY(capturedImage.getY() + cutOutStartY);
+            cutOutRegion.setWidth(cutOutEndX - cutOutStartX);
+            cutOutRegion.setHeight(cutOutRegion.getWidth());
+        }
+    };
+
+    private EventHandler<? super MouseEvent> mouseReleasedHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            try {
+                System.out.println("RELEASED!");
+                int cutOutWidth = (int) (cutOutEndX - cutOutStartX);
+                int cutOutHeight = (int) (cutOutEndY - cutOutStartY);
+                BufferedImage bImage = ImageIO.read(new File(currOriginName)).getSubimage((int) cutOutStartX, (int) cutOutStartY, cutOutWidth, cutOutHeight);
+                File outputfile = new File(currOriginName.replace(".", "_ROI."));
+                ImageIO.write(bImage, "jpeg", outputfile);
+            } catch (IOException e) {
+                System.out.println("An error occured.");
+            }
+        }
+    };
+
+    private ChangeListener<? super Number> sceneSizeChangedListener = new ChangeListener<Number>() {
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+            capturedImage.setFitWidth(newSceneWidth.doubleValue() / 2);
+            cutOutRegion.setX(cutOutRegion.getX() * (newSceneWidth.doubleValue() / oldSceneWidth.doubleValue()));
+            cutOutRegion.setY(cutOutRegion.getWidth() * (newSceneWidth.doubleValue() / oldSceneWidth.doubleValue()));
+            cutOutRegion.setWidth(cutOutRegion.getWidth() * (newSceneWidth.doubleValue() / oldSceneWidth.doubleValue()));
+            cutOutRegion.setHeight(cutOutRegion.getWidth());
+        }
+    };
 
     @Override
     public void init() {
-        cutOutRegion = RectangleBuilder.create()
-                .x(50)
-                .y(50)
-                .width(100)
-                .height(100)
-                .build();
+        cutOutRegion = RectangleBuilder.create().build();
     }
 
     @Override
     public void start(Stage stage) {
-        StackPane pane = new StackPane();
+        Group root = new Group();
         capturedImageSrc = getMostRecentOrigin();
         capturedImage = new ImageView();
         capturedImage.setImage(capturedImageSrc);
         capturedImage.setPreserveRatio(true);
-        capturedImage.setFitHeight(200.0);
-        capturedImage.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("CLICK!");
-                cutOutStartX = mouseEvent.getX();
-                cutOutStartY = mouseEvent.getY();
-            }
-        });
+        capturedImage.setOnMousePressed(mousePressedHandler);
 
-        capturedImage.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("DRAGGED!");
-                cutOutEndX = mouseEvent.getX();
-                cutOutEndY = mouseEvent.getY();
+        capturedImage.setOnMouseDragged(mouseDraggedHandler);
 
-                cutOutRegion.setX(cutOutStartX - capturedImage.getFitWidth());
-                cutOutRegion.setY(cutOutStartY- capturedImage.getFitHeight());
-                cutOutRegion.setWidth(cutOutEndX - cutOutStartX);
-                cutOutRegion.setWidth(cutOutEndY - cutOutStartY);
-            }
-        });
-
-        capturedImage.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("RELEASED!");
-
-            }
-        });
-        pane.getChildren().add(capturedImage);
-        pane.getChildren().add(cutOutRegion);
-        Scene scene = new Scene(pane, DIM_X, DIM_Y);
+        capturedImage.setOnMouseReleased(mouseReleasedHandler);
+        root.getChildren().add(capturedImage);
+        root.getChildren().add(cutOutRegion);
+        Scene scene = new Scene(root, DIM_X, DIM_Y);
+        scene.widthProperty().addListener(sceneSizeChangedListener);
 
         stage.setScene(scene);
         stage.show();
+        capturedImage.setFitWidth(stage.getWidth() / 2);
     }
 
     private Image getMostRecentOrigin() {
@@ -93,6 +119,7 @@ public class Main extends Application {
                 mostRecentOrigin = f;
             }
         }
+        currOriginName = dir.getName() + "/" + mostRecentOrigin.getName();
         return new Image("file:" + dir.getName() + "/" + mostRecentOrigin.getName());
     }
 
